932f0d178f511b8a2cc6e8e5b37d49be82f82b06
Wafaa Soliman <eng.wafaa.soliman@gmail.com>
2020-09-22 17:02:08 +0200

    first commit for all files

---
+++ products/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from .models import Product
+
+# Create your views here.
+def product(request):
+    return render(request, 'products/product.html')
+def products(request):
+    context = {
+        'products': Product.objects.all()
+    }
+    return render(request, 'products/products.html', context)
+
+def search(request):
+    return render(request, 'products/search.html')
