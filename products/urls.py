49ef3197bbba24057c2dbcce8d69e89481c1e231
Wafaa Soliman <eng.wafaa.soliman@gmail.com>
2020-10-03 15:23:31 +0200

    push new changes of project

--- products/urls.py
+++ products/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('product', views.product, name='product'),
+    path('<int:pro_id>', views.product, name='product'),
     path('products', views.products, name='products'),
     path('search', views.search, name='search'),
 ]
